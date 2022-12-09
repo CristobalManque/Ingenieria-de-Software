dd670a8bc1458e5c90a6a225cb4b4ed8e18f7582
Sebastián Gallegos F <59512190+SebaGallegos@users.noreply.github.com>
2022-12-09 15:54:30 -0300

    Cambios en tablas y metodos para leer base de datos
    Co-Authored-By: CristobalManque <114581013+CristobalManque@users.noreply.github.com>

--- mysite/db.py
+++ mysite/db.py
@@ -1,22 +1,25 @@
-import sqlite3 as sql
+import sqlite3
 import click
 from flask import current_app, g
 
+
 def get_db():
     if 'db' not in g:
-        g.db = sql.connect(
+        g.db = sqlite3.connect(
             current_app.config['DATABASE'],
-            detect_types=sql.PARSE_DECLTYPES
+            detect_types=sqlite3.PARSE_DECLTYPES
         )
-        g.db.row_factory = sql.Row
+        g.db.row_factory = sqlite3.Row
 
     return g.db
+
 
 def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:    
         db.close()
+        
 
 def init_db():
     db = get_db()
@@ -24,12 +27,24 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
+def get_db_row():
+    conn = sqlite3.connect(current_app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def read_db_row(table):
+    conn = get_db_row()
+    data = conn.execute(f'SELECT * FROM {table}').fetchall();
+    return data
+
+def read_db_col(table, col):
+    conn = get_db_row()
+    data = conn.execute(f'SELECT * FROM {table} WHERE {col} = {col}').fetchall();
+    return data
+
 @click.command('init-db')
 def init_db_command():
-    """
-    Borra los datos existentes e inicializa la base de datos
-    Para ejecutar: flask --app mysite init-db
-    """
+    """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
     
