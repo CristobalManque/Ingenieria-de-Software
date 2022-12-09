6d6bba5e7c3beba4bd00f95a9b4ce0837556b808
Sebastián Gallegos F <59512190+SebaGallegos@users.noreply.github.com>
2022-12-09 15:56:02 -0300

    Ingreso y listado de productos
    Co-Authored-By: NikoMalek <114967158+NikoMalek@users.noreply.github.com>
    Co-Authored-By: Camilo Ojeda <114362516+camilo-ojedav@users.noreply.github.com>

--- mysite/views.py
+++ mysite/views.py
@@ -7,4 +7,41 @@ bp = Blueprint('index', __name__)
 @bp.route('/index', methods=['GET'])
 @auth.login_required
 def index():
-    return render_template("index.html")
+    return render_template("index.html")@auth.login_required
+def ingresarproducto():
+    if request.method == 'POST':
+        sku = request.form['sku']
+        nombre = request.form['nombre']
+        fecha = request.form['fecha']
+        cantidad = request.form['cantidad']
+        preciou = request.form['preciou']
+
+        db = get_db()
+        error = None
+
+        if not sku:
+            error = "La sku es requerida"
+        elif not nombre:
+            error = "El nombre es requerido"
+        elif not fecha:
+            error = "El fecha es requerido"
+        elif not cantidad:
+            error = "El cantidad es requerido"
+        elif not preciou:
+            error = "El preciou es requerido"
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO producto (sku, nombre, fechaentrega, cantidad, precioU) VALUES (?, ?, ?, ?, ?)",
+                    (sku, nombre, fecha, cantidad, preciou),
+                )
+                db.commit()
+            except:
+                error = "Error"
+                return render_template("ingresarproducto.html", error=error)
+            else:
+                return redirect(url_for("vista.inicio"))
+
+    return render_template("ingresarproducto.html")
+
