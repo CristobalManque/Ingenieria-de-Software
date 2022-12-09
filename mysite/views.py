765906c1311e3070c5514fbeb9823e8dc2bf9519
Sebastián Gallegos F <59512190+SebaGallegos@users.noreply.github.com>
2022-12-09 15:58:19 -0300

    Ingreso de ordenes de compra de cliente y proveedor
    Co-Authored-By: cristobalO19 <114106113+cristobalO19@users.noreply.github.com>

--- mysite/views.py
+++ mysite/views.py
@@ -1,13 +1,93 @@
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
+from flask_sqlalchemy import SQLAlchemy
 from . import auth
+from . import db
+from mysite.db import read_db_row, read_db_col
+from mysite.db import get_db
 
-bp = Blueprint('index', __name__)
+bp = Blueprint('vista', __name__)
 
 @bp.route('/', methods=['GET'])
-@bp.route('/index', methods=['GET'])
 @auth.login_required
-def index():
-    return render_template("index.html")@auth.login_required
+def inicio():
+    if g.user['tipo'] == "cliente":
+        return render_template("index.html", items = read_db_row("OCcliente"))
+    else:
+        return render_template("index.html", items = read_db_row("OCproveedor"))
+
+@bp.route('/listaproductos', methods=['GET'])
+@auth.login_required
+def listaproductos():
+    if g.user['tipo'] == "cliente":
+        return render_template("listaproductos.html", items = db.read_db_row("producto"))
+
+@bp.route('/ingresar', methods=('GET', 'POST'))
+@auth.login_required
+def ingresar():
+    if request.method == 'POST':
+        if g.user['tipo'] == "cliente":
+            sku_p = request.form['sku_p']
+            fecha = request.form['fecha']
+            tmoneda = request.form['tmoneda']
+            lugar = request.form['lugar']
+            tpago = request.form['tpago']
+        else:
+            cliente = request.form['cliente']
+            fecha = request.form['fecha']
+            tmoneda = request.form['tmoneda']
+            lugar = request.form['lugar']
+            tpago = request.form['tpago']
+
+        db = get_db()
+        error = None
+
+        if g.user['tipo'] == "cliente":
+            if not sku_p:
+                error = "error en sku"
+            elif not fecha:
+                error = "La fecha es requerida"
+            elif not tmoneda:
+                error = "El tipo de moneda es requerido"
+            elif not lugar:
+                error = "El lugar es requerido"
+            elif not tpago:
+                error = "El termino de pago es requerido"
+        else:
+            if not cliente:
+                error = "Error en cliente"
+            elif not fecha:
+                error = "La fecha es requerida"
+            elif not tmoneda:
+                error = "El tipo de moneda es requerido"
+            elif not lugar:
+                error = "El lugar es requerido"
+            elif not tpago:
+                error = "El termino de pago es requerido"
+
+        if error is None:
+            try:
+                if g.user['tipo'] == "cliente":
+                    db.execute(
+                        "INSERT INTO OCcliente (producto, fecha, tmoneda, lugar, tpago) VALUES (?, ?, ?, ?, ?)",
+                        (sku_p, fecha, tmoneda, lugar, tpago),
+                    )
+                    db.commit()
+                else:
+                    db.execute(
+                        "INSERT INTO OCproveedor (cliente, fecha, tmoneda, lugar, tpago) VALUES (?, ?, ?, ?, ?)",
+                        (cliente, fecha, tmoneda, lugar, tpago),
+                    )
+                    db.commit()
+            except:
+                error = "Error"
+                return render_template("ingresar.html", items = read_db_row("producto"), error=error)
+            else:
+                return redirect(url_for("vista.inicio"))
+
+    if g.user['tipo'] == "cliente":
+        return render_template("ingresar.html", items = read_db_row("producto"))
+    else:
+        return render_template("ingresar.html", items = read_db_row("OCcliente"))
 def ingresarproducto():
     if request.method == 'POST':
         sku = request.form['sku']
